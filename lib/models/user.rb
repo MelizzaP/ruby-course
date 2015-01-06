6e9241043627ea75d8fc6c3f47f23399c3b8c0ad
James Knepper <knepperjb@gmail.com>
2015-01-06 21:03:40 +0000

    add validations to user

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates :username, :presence => true
   validates :password, :presence => true
+  validates :password, confirmation: true
 end
 
 class Dog < ActiveRecord::Base
